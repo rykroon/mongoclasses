76afb5484c7cb0ce7b28d49dbaf21d174f15f485
Ryan Kroon <rykroon.tech@gmail.com>
2023-06-27 08:25:02 -0400

    Updated version to 0.4.0.

--- src/mongoclasses/__about__.py
+++ src/mongoclasses/__about__.py
@@ -1,1 +1,17 @@
-__version__ = "0.3.0"
+__version__ = "0.4.0"
+
+
+"""
+Goals for version 0.4.0
+
+- Add 'asdict' parameter for inserts and updates.
+
+- Additional documentation using mkdocs.
+    - Show how mongoclasses can work well with other packages:
+        - dacite (fromdict)
+        - pydantic (pydantic dataclasses)
+
+    - Show example of creating custom asdict_shallow() function.
+    - Show example of choosing which fields to update using dict_factory.
+
+"""
