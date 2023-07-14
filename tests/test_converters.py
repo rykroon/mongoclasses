5e46161b7ed9241e1c595e8da0bd6a8fef0a08b2
Ryan Kroon <rykroon.tech@gmail.com>
2023-07-14 08:32:57 -0400

    Added additional tests for 100% code coverage.

--- tests/test_converters.py
+++ tests/test_converters.py
@@ -4,6 +4,7 @@ import re
 from uuid import UUID, uuid4
 
 from bson import Binary, Decimal128, ObjectId, Regex, SON
+import pytest
 
 from mongoclasses import converter
 
@@ -15,6 +16,8 @@ class TestConversions:
         assert converter.structure(d, datetime) == d
         assert converter.structure(d.isoformat(), datetime) == d
         assert converter.structure(d.timestamp(), datetime) == d
+        with pytest.raises(TypeError):
+            converter.structure(None, datetime)
 
     def test_decimal(self):
         """
@@ -31,12 +34,17 @@ class TestConversions:
         assert converter.structure(1.23, Decimal) == Decimal.from_float(1.23)
         assert converter.structure(1, Decimal) == Decimal(1)
 
+        with pytest.raises(TypeError):
+            converter.structure(None, Decimal)
     
     def test_decimal128(self):
         d = Decimal("1.23")
         d128 = Decimal128("1.23")
         assert converter.structure(d128, Decimal128) == d128
         assert converter.structure(d, Decimal128) == d128
+
+        with pytest.raises(TypeError):
+            converter.structure(None, Decimal128)
 
     def test_objectid(self):
         o = ObjectId()
@@ -48,13 +56,19 @@ class TestConversions:
         reg = Regex.from_native(p)
         assert converter.structure(p, re.Pattern) == p
         assert converter.structure(reg, re.Pattern) == p
+
+        with pytest.raises(TypeError):
+            converter.structure(None, re.Pattern)
     
     def test_regex(self):
         p = re.compile(".*")
         reg = Regex.from_native(p)
         assert converter.structure(reg, Regex) == reg
         assert converter.structure(p, Regex) == reg
-    
+
+        with pytest.raises(TypeError):
+            converter.structure(None, Regex)
+
     def test_son(self):
         s = SON({"a": 1})
         assert converter.structure(s, SON) == s
@@ -66,3 +80,6 @@ class TestConversions:
         assert converter.structure(str(u), UUID) == u
         assert converter.structure(u.int, UUID) == u
         assert converter.structure(Binary.from_uuid(u), UUID) == u
+
+        with pytest.raises(TypeError):
+            converter.structure(None, UUID)
