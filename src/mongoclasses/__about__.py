62e0641176de6da588e17ba822184f9791e09a21
Ryan Kroon <rykroon.tech@gmail.com>
2023-07-15 10:46:41 -0400

    Updated version to 0.5.0. Added notes for upcoming goals.

--- src/mongoclasses/__about__.py
+++ src/mongoclasses/__about__.py
@@ -1,1 +1,25 @@
-__version__ = "0.4.0"
+__version__ = "0.5.0"
+
+
+"""
+Goals for 0.5.0
+
+- Add conversions.
+    - Dbref
+    - Consider how to handle sets, frozensets, Date, and Time.
+    - Think of a way to handle legacy UUID binary fields.
+
+
+- Add documentation on conversions.
+
+    
+- Potentially add specific update operators to update_one() such as set, unset,
+    currentDate, etc.
+    - For example, unset_none will do an unset for all values that are None.
+
+
+- Add additional operations such as:
+    - find_one_and_delete()
+    - find_one_and_update()
+    - insert_many()
+"""
