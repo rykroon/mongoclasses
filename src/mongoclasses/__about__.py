bd7b2f282d5ef755c54287dcb62b6962220a6dbf
Ryan Kroon <rykroon.tech@gmail.com>
2023-07-15 09:55:31 -0400

    Removed comments.

--- src/mongoclasses/__about__.py
+++ src/mongoclasses/__about__.py
@@ -1,15 +1,1 @@
 __version__ = "0.4.0"
-
-
-"""
-Goals for version 0.4.0
-
-- Possibly rename 'collection' classvar to 'mongodb_collection'.
-    This way the name 'collection' isn't reserved.
-
-- Additional documentation using mkdocs.
-        - pydantic (pydantic dataclasses)
-
-    - Show example of choosing which fields to update.
-
-"""
