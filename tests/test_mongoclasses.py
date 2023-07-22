78272f3833f3bbecbd8d1b2deba6ab6bf71b14ee
Ryan Kroon <rykroon.tech@gmail.com>
2023-07-22 12:07:45 -0400

    Re-did mongoclasses tests.

--- tests/test_mongoclasses.py
+++ tests/test_mongoclasses.py
@@ -1,90 +1,79 @@
-from dataclasses import dataclass
-from typing import Any
+from dataclasses import dataclass, is_dataclass, field
 
-from mongoclasses import mongoclass, is_mongoclass, _is_mongoclass_instance
+from pymongo import MongoClient
+import pytest
+
+from mongoclasses import mongoclass, is_mongoclass
+
+
+@pytest.fixture
+def client():
+    return MongoClient()
+
+
+class TestMongoclass:
+
+    def test_not_a_class(self, client):
+        with pytest.raises(TypeError):
+            mongoclass(db=client["test"])(None)
+
+    def test_missing_id_field(self, client):
+        with pytest.raises(TypeError):
+            @mongoclass(db=client["test"])
+            class MyClass:
+                ...
+
+    def test_id_field_with_alias(self, client):
+        # should not raise a TypeError.
+        @mongoclass(db=client["test"])
+        class MyClass:
+            id: int = field(metadata={"mongoclasses": {"db_name": "_id"}})
+
+    def test_make_dataclass(self, client):
+        @mongoclass(db=client["test"])
+        class MyClass:
+            _id: int
+
+        assert is_dataclass(MyClass) is True
+
+    def test_default_collection_name(self, client):
+        @mongoclass(db=client["test"])
+        class MyClass:
+            _id: int
+        
+        assert MyClass.__mongoclass_collection__.name == "myclass"
+    
+    def test_custom_collection_name(self, client):
+        @mongoclass(db=client["test"], collection_name="my_class")
+        class MyClass:
+            _id: int
+        
+        assert MyClass.__mongoclass_collection__.name == "my_class"
 
 
 class TestIsMongoclass:
 
     """
-    Tests for the is_mongoclass(), _is_mongoclass_instance(),
-    and _is_mongoclass_type() functions.
+    Tests for the is_mongoclass()
     """
 
-    def test_not_a_dataclass(self):
+    def test_just_a_class(self):
         class MyClass:
             ...
 
         assert is_mongoclass(MyClass) is False
-        assert is_mongoclass(MyClass()) is False
-
-        assert _is_mongoclass_instance(MyClass) is False
-        assert _is_mongoclass_instance(MyClass()) is False
-
-    def test_no_id_and_no_collection(self):
+    
+    def test_just_a_dataclass(self):
         @dataclass
         class MyClass:
             ...
-
+        
         assert is_mongoclass(MyClass) is False
-        assert is_mongoclass(MyClass()) is False
+    
+    def test_is_a_mongoclass(self, client):
+        @mongoclass(db=client["test"])
+        class MyClass:
+            _id: int = 0
 
-        assert _is_mongoclass_instance(MyClass) is False
-        assert _is_mongoclass_instance(MyClass()) is False
-
-    # def test_no_collection(self):
-    #     @dataclass
-    #     class MyClass:
-    #         _id: Any = None
-        
-    #     assert is_mongoclass(MyClass) is False
-    #     assert is_mongoclass(MyClass()) is False
-
-    #     assert _is_mongoclass_instance(MyClass) is False
-    #     assert _is_mongoclass_instance(MyClass()) is False
-
-    # def test_no_id(self):
-    #     @dataclass
-    #     class MyClass:
-    #         collection: ClassVar[AsyncIOMotorCollection]
-        
-    #     assert is_mongoclass(MyClass) is False
-    #     assert is_mongoclass(MyClass()) is False
-
-    #     assert _is_mongoclass_instance(MyClass) is False
-    #     assert _is_mongoclass_instance(MyClass()) is False
-
-    # def test_incorrect_field_type_for_id(self):
-    #     @dataclass
-    #     class MyClass:
-    #         collection: ClassVar[AsyncIOMotorCollection] = None
-    #         _id: InitVar[Any] = None
-        
-    #     assert is_mongoclass(MyClass) is False
-    #     assert is_mongoclass(MyClass()) is False
-
-    #     assert _is_mongoclass_instance(MyClass) is False
-    #     assert _is_mongoclass_instance(MyClass()) is False
-
-    # def test_incorrect_field_type_for_collection(self):
-    #     @dataclass
-    #     class MyClass:
-    #         collection: AsyncIOMotorCollection = None
-    #         _id: Any = None
-        
-    #     assert is_mongoclass(MyClass) is False
-    #     assert is_mongoclass(MyClass()) is False
-
-    #     assert _is_mongoclass_instance(MyClass) is False
-    #     assert _is_mongoclass_instance(MyClass()) is False
-
-    # def test_success(self):
-    #     @mongoclass()
-    #     class MyClass:
-    #         _id: Any = None
-        
-    #     assert is_mongoclass(MyClass) is True
-    #     assert is_mongoclass(MyClass()) is True
-
-    #     assert _is_mongoclass_instance(MyClass) is False
-    #     assert _is_mongoclass_instance(MyClass()) is True
+        assert is_mongoclass(MyClass) is True
+        assert is_mongoclass(MyClass()) is True
