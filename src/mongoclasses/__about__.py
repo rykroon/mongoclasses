66ddddb3e950e7ec7ac86d4f8622e017092366cf
Ryan Kroon <rykroon.tech@gmail.com>
2023-07-22 12:23:43 -0400

    Updated notes in about.

--- src/mongoclasses/__about__.py
+++ src/mongoclasses/__about__.py
@@ -12,11 +12,6 @@ Goals for 0.5.0
 
 - Add documentation on conversions.
 
-    
-- Potentially add specific update operators to update_one() such as set, unset,
-    currentDate, etc.
-    - For example, unset_none will do an unset for all values that are None.
-
 
 - Add additional operations such as:
     - find_one_and_delete()
