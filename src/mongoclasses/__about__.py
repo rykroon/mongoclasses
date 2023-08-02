1e944dff20f485dbedf0ab0b289300743dbc39d5
Ryan Kroon <rykroon.tech@gmail.com>
2023-08-02 17:42:22 -0400

    Removed comments regarding conversions.

--- src/mongoclasses/__about__.py
+++ src/mongoclasses/__about__.py
@@ -4,15 +4,6 @@ __version__ = "0.5.0"
 """
 Goals for 0.5.0
 
-- Add conversions.
-    - Dbref
-    - Consider how to handle sets, frozensets, Date, and Time.
-    - Think of a way to handle legacy UUID binary fields.
-
-
-- Add documentation on conversions.
-
-
 - Add additional operations such as:
     - find_one_and_delete()
     - find_one_and_update()
