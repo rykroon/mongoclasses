22f2697db92a3505f9ecedee6936c94624735aea
Ryan Kroon <rykroon.tech@gmail.com>
2023-08-15 20:00:01 -0400

    optimized recursive update expression function.

--- src/mongoclasses/update.py
+++ src/mongoclasses/update.py
@@ -1,114 +1,101 @@
 from dataclasses import dataclass, fields, is_dataclass
-from typing import Any, Literal, Union
-
-from .serialization import to_document
+from typing import Any, ClassVar, List, Literal, Union
 
 
 class UpdateOperator:
-    pass
+    key: ClassVar[str]
+    value: Any
 
 
 @dataclass
 class CurrentDate(UpdateOperator):
-    type_specification: Literal["timstamp", "date"] = "date"
+    key: ClassVar[str] = "$currentDate"
+    value: Literal["timstamp", "date"] = "date"
+
+    def __post_init__(self):
+        self.value = {"$type": self.value}
 
 
 @dataclass
 class Inc(UpdateOperator):
-    amount: Union[int, float]
+    key: ClassVar[str] = "$inc"
+    value: Union[int, float]
 
 
 @dataclass
 class Min(UpdateOperator):
+    key: ClassVar[str] = "$min"
     value: Any
 
 
 @dataclass
 class Max(UpdateOperator):
+    key: ClassVar[str] = "$max"
     value: Any
 
 
 @dataclass
 class Mul(UpdateOperator):
-    number: Union[int, float]
+    key: ClassVar[str] = "$mul"
+    value: Union[int, float]
 
 
 @dataclass
-class Unset:
-    pass
+class Rename(UpdateOperator):
+    key: ClassVar[str] = "$rename"
+    value: str
 
 
-class UpdateExpr(dict):
-
-    @property
-    def current_date(self):
-        return self.setdefault("$currentDate", {})
-
-    @property
-    def inc(self):
-        return self.setdefault("$inc", {})
-
-    @property
-    def min(self):
-        return self.setdefault("$min", {})
-
-    @property
-    def max(self):
-        return self.setdefault("$max", {})
-
-    @property
-    def mul(self):
-        return self.setdefault("$mul", {})
-
-    @property
-    def set(self):
-        return self.setdefault("$set", {})
-    
-    @property
-    def unset(self):
-        return self.setdefault("$unset", {})
+@dataclass
+class Unset(UpdateOperator):
+    key: ClassVar[str] = "$unset"
+    value: Any = ""
 
 
-def recursive_update(mapping, *new_mappings):
-    for new_mapping in new_mappings:
-        for k, v in new_mapping.items():
-            if k in mapping and isinstance(mapping[k], dict) and isinstance(v, dict):
-                mapping[k] = recursive_update(mapping[k], v)
-            else:
-                mapping[k] = v
-    return mapping
+@dataclass
+class AddToSet(UpdateOperator):
+    key: ClassVar[str] = "$addToSet"
+    value: Any
+
+
+@dataclass
+class Pop(UpdateOperator):
+    key: ClassVar[str] = "$pop"
+    value: Literal[-1, 1]
+
+
+@dataclass
+class Pull(UpdateOperator):
+    key: ClassVar[str] = "$pull"
+    value: Any
+
+
+@dataclass
+class Push(UpdateOperator):
+    key: ClassVar[str] = "$push"
+    value: Any
+
+
+@dataclass
+class PullAll(UpdateOperator):
+    key: ClassVar[str] = "$pullAll"
+    value: List[Any]
 
 
 def to_update_expr(obj, /, update_fields=None):
-    result = UpdateExpr()
+    result = {}
     return any_to_update_expr(result, None, obj)
 
 
 def any_to_update_expr(result, path, obj, update_fields=None):
     if isinstance(obj, UpdateOperator):
-        if isinstance(obj, CurrentDate):
-            result.current_date[path] = {"$type": obj.type_specification}
-
-        elif isinstance(obj, Inc):
-            result.inc[path] = obj.amount
-
-        elif isinstance(obj, Min):
-            result.min[path] = obj.value
-
-        elif isinstance(obj, Max):
-            result.max[path] = obj.value
-
-        elif isinstance(obj, Mul):
-            result.mul[path] = obj.number
-
-        elif isinstance(obj, Unset):
-            result.unset[path] = {path: ""}
+        result.setdefault(obj.key, {})[path] = obj.value
 
     elif is_dataclass(obj):
-        for fld in fields(obj):
-            field_name = fld.metadata.get("mongoclasses", {}).get("db_field", fld.name)
-            field_path = field_name if path is None else f"{path}.{field_name}"
-            v = getattr(obj, fld.name)
+        for field in fields(obj):
+            db_field = field.metadata.get("mongoclasses", {}).get("db_field", field.name)
+            field_path = db_field if path is None else f"{path}.{db_field}"
+            v = getattr(obj, field.name)
             any_to_update_expr(result, field_path, v)
 
     elif isinstance(obj, (list, tuple)):
@@ -120,6 +107,6 @@ def any_to_update_expr(result, path, obj, update_fields=None):
             any_to_update_expr(result, f"{path}.{k}", v)
 
     else:
-        result.set[path] = obj
+        result.setdefault("$set", {})[path] = obj
 
     return result
