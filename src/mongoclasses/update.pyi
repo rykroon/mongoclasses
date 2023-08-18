4628cfd5afaa7c43aeb793f70d4e098e513f003c
Ryan Kroon <rykroon.tech@gmail.com>
2023-08-17 21:27:49 -0400

    Added type annotations for to_update_expr().

--- src/mongoclasses/update.pyi
+++ src/mongoclasses/update.pyi
@@ -1,5 +1,11 @@
 
 from typing import Any
+from .mongoclasses import MongoclassInstance
+
+def to_update_expr(
+    obj: MongoclassInstance, /, include: list[str] | None=None
+) -> dict[str, Any]:
+    pass
 
 def any_to_update_expr(
     result: dict[str, Any], path: str | None, obj: Any, include: list[str] | None=None
