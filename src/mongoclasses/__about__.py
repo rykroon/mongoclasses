7ee64985235437db8c8f0b5beef1c7a303dae7b1
Ryan Kroon <rykroon.tech@gmail.com>
2023-08-17 21:20:36 -0400

    Added list of to-dos for version 0.6.0.

--- src/mongoclasses/__about__.py
+++ src/mongoclasses/__about__.py
@@ -1,1 +1,21 @@
 __version__ = "0.6.0"
+
+
+"""
+Goals for 0.6.0
+
+- Possibly reorganize project structure.
+
+- Add additional tests for each update operator.
+
+- Add update operator modifiers.
+  - https://www.mongodb.com/docs/manual/reference/operator/update-array/#update-operator-modifiers
+
+- Add upsert logic to replace_one().
+- Add upsert logic to update_one().
+
+- Add more detailed examples in Documentation.
+- Add https to mongoclasses.rykroon.com
+
+
+"""
